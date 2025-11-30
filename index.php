f90d650bf524e48c1903ca3069b13ce4062a539c
Thomas Deseure <deseur.t@gmail.com>
2025-11-30 15:21:31 +0100

    Fix: Commented the command line in `index.php`: `require_once __DIR__ . "/api/auth/login.php";`

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . "/api/auth/login.php";
+// require_once __DIR__ . "/api/auth/login.php";
 require_once __DIR__ . "/api/helper/debug.php";
 require_once __DIR__ . "/db/database.php";
 require_once __DIR__ . "/db/FaithGuardRepository.php";  // Uncomment if needed for DB operations
