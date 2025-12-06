3b66b58d1c6115cca2f88e29ad62caa6eac0d9fe
Thomas Deseure <deseur.t@gmail.com>
2025-12-06 21:12:28 +0100

    Fix: linking the auth.js instead of the deleted nav.js

--- index.php
+++ index.php
@@ -288,7 +288,7 @@
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" xintegrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 <!-- Custom JS -->
-<script src="assets/js/nav.js"></script>
+<script src="assets/js/auth.js"></script>
 <script src="assets/js/footer.js"></script>
 <script src="assets/js/resources.js"></script>
 </html>
