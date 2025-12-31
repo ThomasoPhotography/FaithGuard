747912a0947424ec49da853fb485431ffbfae71d
Thomas Deseure <deseur.t@gmail.com>
2025-12-31 21:06:00 +0100

    Fix: Linking to db

--- api/services/quizTimelineService.php
+++ api/services/quizTimelineService.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../db/FaithGuardRepository.php';
+require_once __DIR__ . '/../../db/FaithGuardRepository.php';
 
     /*=========================================================
         QUIZ TIMELINE SERVICE
